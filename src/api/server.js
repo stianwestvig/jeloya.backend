72d7741474045f87b741c3e83339f581206b86a1
Per Atle Holvik <per.holvik@bouvet.no>
2015-11-21 15:30:11 +0100

    api-endepunkt for å registre bruker

--- src/api/server.js
+++ src/api/server.js
@@ -7,6 +7,7 @@
 var express = require("express");
 var http = require("http");
 var moment = require("moment");
+var crypto = require("crypto");
 
 var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
@@ -19,39 +20,63 @@ var path = require("path");
 
 var parkings = [];
 var pricePerSecond = 1;
+var customers = [];
 
-var findCurrentParking = function(user){
-    for(var i = 0; i < parkings.length; i++) {
-        if(parkings[i].user === user && !isEndedParking(parkings[i]))  {
+var findCurrentParking = function (user) {
+    for (var i = 0; i < parkings.length; i++) {
+        if (parkings[i].user === user && !isEndedParking(parkings[i])) {
             return parkings[i];
         }
     }
 };
 
-var isEndedParking = function(parking){
+var isEndedParking = function (parking) {
     return typeof(parking.end) !== "undefined";
 }
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
+var allowCrossDomain = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 };
 
 app.use(allowCrossDomain);
 
+var generateGuid = function () {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+        var r = crypto.randomBytes(1)[0] % 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    })
+};
+
 // body parser - to get parameters from post request:
 /*app.use(bodyParser.urlencoded({
-    extended: true
-}));*/
+ extended: true
+ }));*/
 
 // ---- Routes ----
+
+app.post("/api/register", jsonParser, function (req, res) {
+    console.log("POST /api/register. Body: ", req.body);
+    if (!req.body) {
+        var missingBody = {"error": "missing body"};
+        console.log("Response: ", missingBody);
+        return res.status(400).send(missingBody);
+    }
+    var guid = generateGuid();
+    console.log("Guid is: ", guid);
+
+    var newCustomer = {name: req.body.name, email: req.body.email, id: guid};
+    customers.push(newCustomer)
+    console.log("Response: ", newCustomer);
+    return res.status(200).send(newCustomer);
+});
+
 app.get("/api/price", jsonParser, function (req, res) {
     console.log("GET /api/price");
     console.log("Response: ", pricePerSecond);
-    return res.status(200).send({ price: pricePerSecond});
+    return res.status(200).send({price: pricePerSecond});
 });
 
 app.post("/api/price", jsonParser, function (req, res) {
@@ -64,7 +89,7 @@ app.post("/api/price", jsonParser, function (req, res) {
 
     pricePerSecond = req.body.price;
     console.log("Response: ", pricePerSecond);
-    return res.status(200).send({ price: pricePerSecond});
+    return res.status(200).send({price: pricePerSecond});
 });
 
 app.get("/api/parked", jsonParser, function (req, res) {
@@ -87,7 +112,7 @@ app.post("/api/park", jsonParser, function (req, res) {
         console.log("Response: ", alreadyParked);
         return res.status(400).send(alreadyParked);
     }
-    
+
     var parking = {
         user: req.body.user,
         start: moment()
@@ -105,15 +130,15 @@ app.post("/api/endpark", jsonParser, function (req, res) {
         console.log("Response: ", missingBody);
         return res.status(400).send(missingBody);
     }
-    
+
     var currentParking = findCurrentParking(req.body.user);
-    
+
     if (!currentParking) {
         var notParked = {"error": "not parked"};
         console.log("Response: ", notParked);
         return res.status(400).send(notParked);
     }
-    
+
     var parking = currentParking;
     parking.end = moment();
     console.log("Response: ", JSON.stringify(parking));

be92c556cd70ac1331c31a9c0e7ce33cfa28dea5
Stian Westvig <stian.westvig@bouvet.no>
2015-11-21 13:13:19 +0100

    added new dash app

---
+++ src/dash/components/logo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Logo extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return(
+            <div>
+                <a href="/"><img src="" alt="" /></a>
+            </div>
+        );
+    }
+}
+
+Logo.propTypes = {};
---
+++ src/dash/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from ''
+
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = { n: 0 }
+  }
+  render () {
+    return(<div>
+        <h1>clicked {this.state.n} times</h1>
+        <button onClick={this.handleClick.bind(this)}>click me!</button>
+    </div>)
+  }
+  handleClick () {
+    this.setState({ n: this.state.n + 1 })
+  }
+}
+
+
+React.render(<App />, document.querySelector('#content'))

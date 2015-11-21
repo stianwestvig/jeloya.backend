8e5f30db2fa44363b3f3fdb36cd112833ebf4625
Per Atle Holvik <per.holvik@bouvet.no>
2015-11-21 14:21:03 +0100

    feilmelding dersom forsøk på å parkere for user som alt er parkert eller avslutte parkering som ikke finnes. mye mer console-logging

--- src/api/server.js
+++ src/api/server.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
 //require('babel/register')({
 //    stage: 0
 //});
 
-var express = require('express');
-var http = require('http');
-var moment = require('moment');
+var express = require("express");
+var http = require("http");
+var moment = require("moment");
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
 
 var PORT = process.env.PORT || 3000;
 
 var app = exports.app = express();
 var server = http.createServer(app);
-var path = require('path');
+var path = require("path");
 
 var parkings = [];
 
@@ -26,43 +26,70 @@ var parkings = [];
 
 // ---- Routes ----
 
-app.get('/api/parked', jsonParser, function (req, res) {
+var findCurrentParking = function(user){    
+    for(var i = 0; i < parkings.length; i++) {       
+        if(parkings[i].user === user && !isEndedParking(parkings[i]))  {
+            return parkings[i];
+        }
+    }
+};
+
+var isEndedParking = function(parking){
+    return typeof(parking.end) !== "undefined";
+}
+
+app.get("/api/parked", jsonParser, function (req, res) {
+    console.log("GET /api/parked");
+    console.log("Response: ", JSON.stringify(parkings));
     return res.status(200).send(parkings);
 });
 
-app.post('/api/park', jsonParser, function (req, res) {
+app.post("/api/park", jsonParser, function (req, res) {
+    console.log("POST /api/park. Body: ", req.body);
     if (!req.body) return res.status(400).send({"error": "missing body"});
-    console.log('got data:', req.body);
+
+    var currentParking = findCurrentParking(req.body.user);
+    if (currentParking) {
+        var alreadyParked = {"error": "already parked"};
+        console.log("Response: ", alreadyParked);
+        return res.status(400).send(alreadyParked);
+    }
+    
     var parking = {
         user: req.body.user,
         start: moment()
     };
 
     parkings.push(parking);
+    console.log("Response: ", JSON.stringify(parking));
     return res.status(200).send(parking);
 });
 
-app.post('/api/endpark', jsonParser, function (req, res) {
-    if (!req.body) return res.status(400).send({"error": "missing body"});
-    var parking;
-    for(var i = 0; i < parkings.length; i++) {
-        if(parkings[i].user === req.body.user) {
-            parking = parkings[i];
-            break;
-        }
+app.post("/api/endpark", jsonParser, function (req, res) {
+    console.log("POST /api/endpark. Body: ", req.body);
+    if (!req.body) {
+        var missingBody = {"error": "missing body"};
+        console.log("Response: ", alreadyParked);
+        return res.status(400).send(missingBody);
     }
-
-    if (typeof(parking) === 'undefined') {
-        res.status(400);
+    
+    var currentParking = findCurrentParking(req.body.user);
+    
+    if (!currentParking) {
+        var notParked = {"error": "not parked"};
+        console.log("Response: ", notParked);
+        return res.status(400).send(notParked);
     }
-
+    
+    var parking = currentParking;
     parking.end = moment();
+    console.log("Response: ", JSON.stringify(parking));
     return res.status(200).json(parking);
 });
 
 
-// start server
+// start 
 server.listen(PORT, function () {
-    var serverAddress = server.address().address === '0.0.0.0' ? 'localhost' : server.address().address;
-    console.log('Server listening on: http://%s:%s', serverAddress, server.address().port);
+    var serverAddress = server.address().address === "0.0.0.0" ? "localhost" : server.address().address;
+    console.log("Server listening on: http://%s:%s", serverAddress, server.address().port);
 });
